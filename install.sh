08179896b9763d0cdc4d14a3ba1f7ca337b675b9
swt02026 <swt02026@yahoo.com.tw>
2015-12-21 19:49:38 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#for vim 
+git clone https://github.com/swt02026/.vim.git ~/.vim
+cd ~/.vim
+git submoudle init 
+git submodule update --init --recursive
+ln -s ~/.vim/vimrc ~/.vimrc
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --system-libclang
+mkdir ~/ycm_build
+cd ~/ycm_build
+cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake --build . --target ycm_support_libs --config Release
+cd
