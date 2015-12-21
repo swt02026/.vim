0b6d7c9a8ed8b577453fc3627fc145a46b857eff
swt02026 <swt02026@yahoo.com.tw>
2015-12-21 20:13:55 +0800

    install ternjs

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ mkdir ~/ycm_build
 cd ~/ycm_build
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 cmake --build . --target ycm_support_libs --config Release
+cd ~/.vim/bundle/tern_for_vim && npm install
 cd
