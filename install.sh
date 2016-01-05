d2cdfed52813dd0a085bbc81b7a71f8c301da124
swt02026 <swt02026@yahoo.com.tw>
2016-01-05 19:15:15 +0800

    order of command

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ cd ~/.vim
 git submoudle init 
 git submodule update --init --recursive
 ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --system-libclang
 mkdir ~/ycm_build
 cd ~/ycm_build
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 cmake --build . --target ycm_support_libs --config Release
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --system-libclang
 cd ~/.vim/bundle/tern_for_vim && npm install
 cd
